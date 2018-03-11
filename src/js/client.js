436bf242badb2dfc38166d4cc11e612d3d2fedf3
dancinoman <dancinoman@gmail.com>
2018-03-11 14:33:15 -0400

    Router react

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Layout from "./components/Layout";
+var React = require("react");
+var ReactDOM = require("react-dom");
+var ReactRouter = require("react-router-dom");
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+
+var Popular = require("./pages/Popular");
+var Nav = require("./pages/Nav");
+
+
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<div className='container'>
+					<Route component={Nav}/>
+				</div>
+			</Router>
+
+		)
+	}
+}
 
 const app = document.getElementById("app");
 
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<App/>, app);
---
+++ src/js/pages/Popular.js
@@ -0,0 +1,13 @@
+var React = require("react");
+
+class Popular extends React.Component {
+	render() {
+		return (
+			<h1>Im Popular yo!</h1>
+
+		)
+	}
+}
+
+
+module.exports = Popular;
